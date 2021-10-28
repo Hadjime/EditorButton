4095a0748e4caecd91d3d4443e4bc359f9ec2fd7
Rasamaha <GavrilovRoman@inbox.ru>
2021-10-28 16:27:17 +0700

    Init

---
+++ EditorButton/EditorButtonAttribute.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Method)]
+public class EditorButtonAttribute : PropertyAttribute
+{
+    public string text = "";
+    public bool showInfo = true;
+
+    public EditorButtonAttribute()
+    {
+
+    }
+
+    public EditorButtonAttribute(string text, bool showInfo = true)
+    {
+        this.text = text;
+        this.showInfo = showInfo;
+    }
+}
